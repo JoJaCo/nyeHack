b119aed09c3d613ab0b06b687d0466e06009867d
JoJaCo <contrerasjorge226@gmail.com>
2025-01-17 05:36:07 -0800

    an argument is passed in placeOrder(), data is being recived and split into its respected location.

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -1,15 +1,25 @@
 const val TAVERN_NAME = "Taernl's Folly"
 
 fun main(args: Array<String>){
-    placeOrder()
+    placeOrder("Shandy,Dragon’s Breath,5.91")
 
 
 }
 
 
 
-private fun placeOrder(){
+private fun placeOrder(menueData: String){
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
     println("Madrigal speaks with $tavernMaster about their order")
-}
+
+        val data = menueData.split(',')
+        val type = data[0]
+        val name = data[1]
+        val price = data[2]
+        val message = "Madrigal buys a $name ($type) for $price."
+        println(message)
+
+    }
+
+
