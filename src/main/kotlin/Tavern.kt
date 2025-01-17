9de2e73c0c649441f27221442e4961f94b9529ba
JoJaCo <contrerasjorge226@gmail.com>
2025-01-17 05:37:54 -0800

    Updated the menueData.split() on line 16

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -13,12 +13,9 @@ private fun placeOrder(menueData: String){
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
     println("Madrigal speaks with $tavernMaster about their order")
 
-        val data = menueData.split(',')
-        val type = data[0]
-        val name = data[1]
-        val price = data[2]
-        val message = "Madrigal buys a $name ($type) for $price."
-        println(message)
+    val (type, name, price) = menueData.split(',')
+    val message = "Madrigal buys a $name ($type) for $price."
+    println(message)
 
     }
 
