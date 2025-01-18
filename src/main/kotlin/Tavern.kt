7abfdf6bf414e691ab22a3dde16ebfc38f4b75bd
JoJaCo <contrerasjorge226@gmail.com>
2025-01-18 01:02:31 -0800

    Created a new function on line 28 also added new code to line 23

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -9,14 +9,31 @@ fun main(args: Array<String>){
 
 
 private fun placeOrder(menueData: String){
+
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+
     println("Madrigal speaks with $tavernMaster about their order")
 
     val (type, name, price) = menueData.split(',')
     val message = "Madrigal buys a $name ($type) for $price."
+
     println(message)
+
+    val phrase = "Ah, delicious $name!"
+    println("Madrigal exclaims: ${toDragonSpeak(phrase)}")
 
     }
 
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiou]")){
+        when (it.value){
+            "a" -> "4"
+            "e" -> "4"
+            "i" -> "4"
+            "o" -> "4"
+            "u" -> "4"
+            else -> it.value
+        }
+    }
 
