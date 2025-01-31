fbb2349e170ecf9b0b00b4a06a75a8a9e58e413e
JoJaCo <contrerasjorge226@gmail.com>
2025-01-31 04:37:41 -0800

    created a fetter and a setter for the player class

--- src/main/kotlin/player.kt
+++ src/main/kotlin/player.kt
@@ -1,6 +1,9 @@
 class player {
-
-    val name = "Madrigal"
+    var name = "Madrigal"
+        get() = field.capitalize()
+        set(value){
+            field = value.trim()
+        }
 
     fun castFireball(numFireballs: Int = 2) =
         println("A glass of Fireball springs into existence (x$numFireballs)")
