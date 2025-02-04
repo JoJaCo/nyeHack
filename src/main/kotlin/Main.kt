88b909bd3c447e2332895d22397ca4795dd3cbc0
JoJaCo <contrerasjorge226@gmail.com>
2025-02-03 23:32:28 -0800

    Started on Chapter 13 Initialization

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,55 +1,30 @@
 fun main(args: Array<String>) {
-    var healthPoints = 89
-    var isBlessed = true
-    var isImmortal = false
+
 
     val player = player()
+    player.name = "estragon"
+    print(player.name + "TheBrave")
     player.castFireball()
 
-    //Auro
-    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
+    //Aura
+    player.auraColor()
+
     println()
-    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+    printPlayerStatus(player)
+
 
     //PlayerStatus
-    printPlayerStatus(auraColor, isBlessed, player.name, healthStatus)
     println()
 
 
 }
 
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
-    when (healthPoints){
-        100 ->"is in excellent condition!"
-        in 90..99 -> "has a few scratches."
-        in 75..89 -> if (isBlessed){
-            "has soome minor wounds, but is healing quite quickly!"
-        }else{
-            "has some minor wounds"
-        }
-        in 15..74 -> "looks pretty hurt"
-        else -> "is in awful condition!"
-    }
-
-
-
-private fun printPlayerStatus(auraColor: String,
-                              isBlessed: Boolean,
-                              name: String,
-                              healthStatus: String){
-    println("(Aura: $auraColor)" +
-            "(Blessed: ${if (isBlessed) "YES" else "NO"})")
-    println("$name $healthStatus")
+private fun printPlayerStatus(player:player){
+    println("(Aura: ${player.auraColor()})" +
+            "(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
+    println("${player.name} ${player.formatHealthStatus()}")
 }
 
-private fun auraColor(isBlessed: Boolean,
-                      healthPoints: Int,
-                      isImomortal: Boolean):String{
-
-    val auraVisible = isBlessed && healthPoints > 50 || isImomortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
-    return auraColor
-}
 
 
 
--- src/main/kotlin/player.kt
+++ src/main/kotlin/player.kt
@@ -5,6 +5,33 @@ class player {
             field = value.trim()
         }
 
+    var healthPoints = 89
+    var isBlessed = true
+    private var isImmortal = false
+
+    fun auraColor():String{
+        val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+        val auraColor = if (auraVisible) "GREEN" else "NONE"
+        return auraColor
+    }
+
+
+    fun formatHealthStatus() =
+        when (healthPoints){
+            100 ->"is in excellent condition!"
+            in 90..99 -> "has a few scratches."
+            in 75..89 -> if (isBlessed){
+                "has soome minor wounds, but is healing quite quickly!"
+            }else{
+                "has some minor wounds"
+            }
+            in 15..74 -> "looks pretty hurt"
+            else -> "is in awful condition!"
+        }
+
+
+
+
     fun castFireball(numFireballs: Int = 2) =
         println("A glass of Fireball springs into existence (x$numFireballs)")
 
