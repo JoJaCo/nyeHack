a6eb7effd0213a8d2b0eb3ad8868edc93da5d72d
JoJaCo <contrerasjorge226@gmail.com>
2025-02-11 22:56:24 -0800

    created our first nested class on line 32 in Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,52 +1,46 @@
 fun main(args: Array<String>) {
 
-
-    val player = player("Madrigal")
-    player.name = "Estragon"
-    print(player.name + "- TheBrave ")
-
-    //Fireball is cast
-    player.castFireball()
-    println()
-
-    var currentRoom: Room =  TownSquare()
-
-    println(currentRoom.description())
-    println(currentRoom.load())
-
-
-
-    println()
-    //Player status
-    printPlayerStatus(player)
     Game.play()
 
-    //Aura
-    player.auraColor()
-
-
-
-    //PlayerStatus
-    println()
-
-
-}
-
-private fun printPlayerStatus(player:player){
-    println("(Aura: ${player.auraColor()})" +
-            "(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
-    println("${player.name} ${player.formatHealthStatus()}")
 }
 
 object Game{
+    private val player = player("Madrigal")
+    private var currentRoom: Room =  TownSquare()
+
+
     init{
         println("Welcome, adventurer.")
+        player.castFireball()
+
     }
 
     fun play(){
         while (true){
             //play NyetHack
+
+            println(currentRoom.description())
+            println(currentRoom.load())
+
+            //Player status
+            printPlayerStatus(player)
+
+            print("> Enter your command:  ")
+            println("Last command: ${readLine()}")
         }
+    }
+    //our nested GameInput class
+    private class GameInput(arg: String?){
+        private val input = arg ?: ""
+        val command = input.split(" ")[0]
+        val argument = input.split(" ").getOrElse(1, {""})
+
+    }
+
+    private fun printPlayerStatus(player:player){
+        println("(Aura: ${player.auraColor()})" +
+                "(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
+        println("${player.name} ${player.formatHealthStatus()}")
     }
 }
 
