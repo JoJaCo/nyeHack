6ad28c5c5a01d00741b73d18182c361cc0f6a830
JoJaCo <contrerasjorge226@gmail.com>
2025-02-14 01:50:38 -0800

    added directions and updatedCoordinate

--- src/main/kotlin/Navigation.kt
+++ src/main/kotlin/Navigation.kt
@@ -1,8 +1,12 @@
-enum class Direction{
-    NORTH,
-    EAST,
-    SOUTH,
-    WEST,
+enum class Direction(private val coordinate: Coordinate){
+    NORTH(Coordinate(0,-1)),
+    EAST(Coordinate(1,0)),
+    SOUTH(Coordinate(1,0)),
+    WEST(Coordinate(-1,0));
+
+    fun updateCoordinate(playerCoordinate: Coordinate) =
+        Coordinate(playerCoordinate.x + coordinate.x,
+            playerCoordinate.y + coordinate.y)
 
 }
 
